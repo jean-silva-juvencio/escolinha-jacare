ff59e73bbbd4fc054e0530a86ead1ab4102ca753
jean-silva-juvencio <volksabel@gmail.com>
2026-05-06 12:53:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
-const CACHE_NAME = 'escolinha-jacare-v1';
+const CACHE_NAME = 'escolinha-v1';
 const urlsToCache = [
-  '/frontend/index.html',
-  '/frontend/diretoria.html',
-  '/frontend/funcionarios.html',
-  '/frontend/prematricula.html',
-  '/frontend/galerias.html',
-  '/frontend/club.html',
-  '/frontend/uniforme.html',
-  '/frontend/comentarios.html',
-  '/frontend/aniversario.html'
+  '/',
+  '/index.html',
+  '/diretoria.html',
+  '/funcionarios.html',
+  '/club.html',
+  '/uniforme.html',
+  '/aniversario.html',
+  '/comentarios.html',
+  '/galerias.html',
+  '/prematricula.html',
+  '/manifest.json',
+  '/icons/jacare.jpeg'
 ];
 
 self.addEventListener('install', event => {
